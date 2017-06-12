030d2874288741c0e1f79ef41c9de16aceba598f
valeriaoshiro <valeriaoshiro19@gmail.com>
2017-06-12 16:35:45 -0700

    Create game logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
 
-cardsInPlay.push(cardOne);
-console.log('User flipped ' + cardsInPlay[0]);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log('User flipped ' + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert('You found a match!');
 	} else {
 		alert('Sorry, try again');
 	}
-}
+};
+
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();		
+	}
+
+};
+
+
+flipCard(0);
+flipCard(2);
